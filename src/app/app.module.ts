180c5cb78504bc621dce6ed0eb9c487e8c1d06dd
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2023-11-06 10:17:24 +0200

    all the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FinalComponent } from './components/final/final.component';
 
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'navigationbar', component: NavigationBarComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'final', component: FinalComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationBarComponent,
+    DashboardComponent,
+    FinalComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatTableModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
